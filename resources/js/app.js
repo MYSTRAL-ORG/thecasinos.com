ffa50a9d0a9c0162b5c2f5928d15cbe13e9b90a2
cisco974 <vitryfrancois@gmail.com>
2023-10-18 16:16:22 +0400

    fix see more when one casino is displayed

--- resources/js/app.js
+++ resources/js/app.js
@@ -398,7 +398,7 @@ console.log("dsq");
                     const zoom = view.getZoom();
                     view.animate({
                         center: feature.getGeometry().getCoordinates(),
-                        zoom: 14
+                        zoom: 18
                     },function() {  // This is the callback after animation completes
                         select.getFeatures().push(feature);
                     });
