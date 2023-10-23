42f51b4abad2abddc21c900f8f3abec604d1e832
cisco974 <vitryfrancois@gmail.com>
2023-10-23 14:47:01 +0400

    creation page casino.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,26 +1,31 @@
-<footer class=" justify-content-center">
-    <div  class="col-sm-6 col-lg-2 ">
-        <a href="https://www.thecasinos.com">
-            <img src="/img/logo.png" alt="Logo">
-        </a>
+
+<div class="container-fluid mt-3 mb-3 ">
+    <div class="row justify-content-center  text-center footer">
+        <div class="col-sm-6 col-lg-2 column ">
+            <a href="https://www.thecasinos.com">
+                <img src="/img/logo.png" alt="Logo">
+            </a>
+        </div>
+        <div class="col-sm-6 col-lg-2 column-description">
+            <p>With TheCasinos.com, enjoy the premier online guide reference. Explore a world of top-tier gaming and enhance your onsite casino journey. Trust in our curated selections to elevate your gaming adventure.</p>
+        </div>
+        <div class="col-sm-6 col-lg-2 column">
+            <a href="about.php">About us</a>
+            <a href="terms.php">Terms and conditions</a>
+            <a href="policy.php">Private policy</a>
+        </div>
+        <div class="col-sm-6 col-lg-2 column">
+            <a href="#" class="btn-download btn-ios">
+                <i class="fab fa-apple" style="margin-right: 10px;"></i> iOS
+            </a>
+            <a href="#" class="btn-download btn-android">
+                <i class="fab fa-android" style="margin-right: 10px;"></i> Android
+            </a>
+        </div>
     </div>
-    <div class="column-description col-sm-6 col-lg-2"  >
-        <p>With TheCasinos.com, enjoy the premier online guide reference. Explore a world of top-tier gaming and enhance your onsite casino journey. Trust in our curated selections to elevate your gaming adventure.</p>
-    </div>
-    <div class="column col-sm-6 col-lg-2">
-        <a href="about.php">About us</a>
-        <a href="terms.php">Terms and conditions</a>
-        <a href="policy.php">Private policy</a>
-    </div>
-    <div class="column  col-sm-6 col-lg-2">
-        <a href="#" class="btn-download btn-ios">
-            <i class="fab fa-apple" style="margin-right: 10px;"></i> iOS
-        </a>
-        <a href="#" class="btn-download btn-android">
-            <i class="fab fa-android" style="margin-right: 10px;"></i> Android
-        </a>
-    </div>
-</footer>
+</div>
+
+
 
 <div class=" copyright-section  d-flex justify-content-between ">
     <div> <p>&copy; 2023 TheCasinos.com - Online reference to onsite experience</p></div>
