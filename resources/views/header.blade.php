62f84f3792a76f3d68b189a275686a01233e1722
cisco974 <vitryfrancois@gmail.com>
2023-10-23 16:29:57 +0400

    mise a jour css

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,34 +12,34 @@
         </button>
 
 
-                <div class="search-bar collapse navbar-collapse  nav_item " >
-                    <input id="search-casino" class=" search-bar "  placeholder="Search..."/>
-                    <ul id="search-casino-list" class="search-casino-list d-none"></ul>
-                    <button class="btn search-btn">
-                        <img src="/img/icons/zoom.png" alt="Search Icon" />
-                    </button>
-                </div>
+        <div class=" search-bar collapse navbar-collapse  nav_item " >
+            <input id="search-casino" class="align-middle  search-bar  "  placeholder="Search..."/>
+            <ul id="search-casino-list" class="search-casino-list d-none"></ul>
+            <button class="btn search-btn">
+                <img src="/img/icons/zoom.png" alt="Search Icon" />
+            </button>
+        </div>
 
-                <div class="language collapse navbar-collapse  nav_item ">
-                    <div class="dropdown">
-                        <button class="btn form-control  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="/img/icons/world.png" alt="World Icon"> EN
-                        </button>
-                        <ul class="dropdown-menu">
-                            <a class="dropdown-item" href="#"><img src="/img/icons/world.png" alt="French Icon"> FR</a>
-                            <a class="dropdown-item" href="#"><img src="/img/icons/world.png" alt="Spanish Icon"> ES</a>
-                            <a class="dropdown-item" href="#"><img src="/img/icons/world.png" alt="Brazilian Icon"> BR</a>
-                        </ul>
-                    </div>
-                </div>
+        <div class="language collapse navbar-collapse  nav_item ">
+            <div class="dropdown">
+                <button class="btn form-control  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="/img/icons/world.png" alt="World Icon"> EN
+                </button>
+                <ul class="dropdown-menu">
+                    <a class="dropdown-item" href="#"><img src="/img/icons/world.png" alt="French Icon"> FR</a>
+                    <a class="dropdown-item" href="#"><img src="/img/icons/world.png" alt="Spanish Icon"> ES</a>
+                    <a class="dropdown-item" href="#"><img src="/img/icons/world.png" alt="Brazilian Icon"> BR</a>
+                </ul>
+            </div>
+        </div>
 
-                <div class=" collapse navbar-collapse nav_item  " role="search">
-                    @if (Route::currentRouteNamed('online'))
-                        <a href="/index2.php"  class="btn-online form-control">ONLINE</a>
-                    @else
-                        <a href="/online.php" class="btn-online form-control">ONLINE</a>
-                    @endif
-                </div>
+        <div class=" collapse navbar-collapse nav_item  " role="search">
+            @if (Route::currentRouteNamed('online'))
+                <a href="/index2.php"  class="btn-online form-control">ONLINE</a>
+            @else
+                <a href="/online.php" class="btn-online form-control">ONLINE</a>
+            @endif
+        </div>
 
     </div>
 </nav>
