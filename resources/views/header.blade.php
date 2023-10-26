8c71d4188f97934666dc6c7912360511c00ec77f
cisco974 <vitryfrancois@gmail.com>
2023-10-26 09:46:02 +0400

    mise a jour css

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,7 +4,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary header ">
     <div class="container-fluid   d-flex justify-content-between">
-        <a  class="navbar-brand  " href="https://www.thecasinos.com/index2.php" alt="TheCasinos.com">
+        <a  class="navbar-brand  " href="/" alt="TheCasinos.com">
             <img id="logo" src="/img/logo-dark.png" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".nav_item" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
