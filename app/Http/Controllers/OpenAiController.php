c88ecc118b284aa57fb48b03e792d780af30e8d2
cisco974 <vitryfrancois@gmail.com>
2023-10-26 08:43:10 +0400

    mise a jour css

--- app/Http/Controllers/OpenAiController.php
+++ app/Http/Controllers/OpenAiController.php
@@ -24,7 +24,7 @@ class OpenAiController extends Controller
     }
 
     public function getListDataToCompute(){
-        $lstCasinoToCompute = DB::table('casino')->where('city_title', '=', 'las-vegas')->whereNotIn('id',CasinoDetailsSource::where('is_done', '=',true)
+        $lstCasinoToCompute = DB::table('casino')->whereNotIn('id',CasinoDetailsSource::where('is_done', '=',true)
             ->pluck('id_casino'))->limit(20)->get();
 
         return response()->json($lstCasinoToCompute);
