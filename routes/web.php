6ac5b17d1a6a61014b48a805f75ab6fbf18ef7eb
cisco974 <vitryfrancois@gmail.com>
2023-10-31 15:22:23 +0400

    page category.blade.php

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', [IndexContoller::class , 'index'])->name('index');
 Route::get('/online',  function () {
     return view('online');  // 'sample' corresponds to the sample.blade.php view file
 })->name('online');
-Route::get('/category/{country}/{city?}', [IndexContoller::class , 'category'])->name('category');
+Route::get('/{country}/{city?}', [IndexContoller::class , 'category'])->name('category');
 
 Route::get('/{country}/{city}/{name}', [LocationController::class , 'show'])->name('casino');
 
