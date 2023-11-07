c1cdec0d26b3eae016132872e0764b64e99a4540
cisco974 <vitryfrancois@gmail.com>
2023-11-07 16:30:24 +0400

    page category.blade.php

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-2">
+    <div class="card">
+        <div class="card-header">
+            Sidebar
+        </div>
+
+        <div class="card-body">
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/category') }}">
+                        Dashboard
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
