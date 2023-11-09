af030baa6feb39b8edbdfe0797accc1f7a1fc8a4
cisco974 <vitryfrancois@gmail.com>
2023-11-09 12:27:28 +0400

    page category.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,17 @@
     <meta name="description" content="@yield('page_description')">
     <meta name="keywords" content="@yield('page_keywords')">
     <title>@yield('context-js')</title>
+
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W6V8TZST');</script>
+    <!-- End Google Tag Manager -->
+
+
 </head>
     <body>
 
