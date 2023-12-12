9cafb9514e731e837009fc172c8dd129e9dd4932
cisco974 <vitryfrancois@gmail.com>
2023-12-12 15:23:37 +0400

    bug casinos.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,7 @@ import {click} from "ol/events/condition.js";
 import {Select} from "ol/interaction.js";
 import $ from "jquery";
 import {containsCoordinate} from "ol/extent.js";
-
+import axios from 'axios';
 
 
 
@@ -175,7 +175,7 @@ $(document).ready(function () {
         });
         async function fetchData() {
             try {
-                const response = await fetch(appUrl+'/casinos.json').then(response => {
+                const response = await axios.get(appUrl+'/casinos.json').then(response => {
                     // Check if the response is ok (status in the range 200-299)
                     if (!response.ok) {
                         throw new Error('Network response was not ok ' + response.statusText);
