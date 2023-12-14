b6a25a4df15c6da324749ad9c3579bf61018e06f
cisco974 <vitryfrancois@gmail.com>
2023-12-14 09:38:39 +0400

    add canonical URL

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('page_title', 'TheCasinos.com : Online reference to Onsite experience')
 
-
+@section('page_description','With TheCasinos.com, enjoy the premier online guide reference. Explore a world of top-tier gaming and enhance your onsite casino journey.')
 @section('map')
     @include('map')
 @endsection
