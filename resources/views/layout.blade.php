db24400c8106592107e3afa627ec4e1563f66743
cisco974 <vitryfrancois@gmail.com>
2024-01-05 14:29:44 +0400

    sitemap.xml

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,7 +28,7 @@
 
 
     <!-- Google Tag Manager -->
-    <script async defer>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script  >(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -36,8 +36,8 @@
     <!-- End Google Tag Manager -->
 
     <!-- Google tag (gtag.js) -->
-    <script async defer src="https://www.googletagmanager.com/gtag/js?id=G-BZTJV1TP3F"></script>
-    <script async defer>
+    <script   src="https://www.googletagmanager.com/gtag/js?id=G-BZTJV1TP3F"></script>
+    <script  >
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
