55329dfd42b042567388b3e6f3860ca7349520b9
cisco974 <vitryfrancois@gmail.com>
2024-03-08 14:47:21 +0400

    update perf 1

--- public/974/breadcrumb-online.php
+++
@@ -1,8 +0,0 @@
-<div class="content-container">
-	<div class="block-location left-block">
-		<div class="block-content">Online</div>
-	</div>
-	<div class="block-location middle-block">
-		<div class="block-content"><a href="#">Casino</a></div>
-	</div>
-</div>
--- resources/views/on-line/menu-casino-online.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="menu-casino">
-	<a href="#sumup" class="menu-btn">Sum up</a>
-	<a href="#bonus" class="menu-btn">Bonus</a>
-	<a href="#deposit" class="menu-btn">Deposit methods</a>
-	<a href="#contact" class="menu-btn">Contact</a>
-</div>
