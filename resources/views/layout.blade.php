e3a89f8b18d8ceb475c51994072b26790c87fadb
cisco974 <vitryfrancois@gmail.com>
2024-03-11 10:52:01 +0400

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,9 +17,9 @@
     <link   rel="preload" href="/css/bootstrap.min.css" as="style" media="all" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="/css/bootstrap.min.css"></noscript>
 
-    <link   rel="stylesheet" href="/css/casinos.css" media="all">
 
-
+    <link   rel="preload" href="/css/casinos.css" as="style" media="all" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="/css/casinos.css"></noscript>
 
 
     <link   rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" as="style" media="all" onload="this.onload=null;this.rel='stylesheet'">
@@ -34,23 +34,7 @@
     <title>@yield('context-js')</title>
 
 
-    <!-- Google Tag Manager -->
-    <script  >(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W6V8TZST');</script>
-    <!-- End Google Tag Manager -->
 
-    <!-- Google tag (gtag.js) -->
-    <script   src="https://www.googletagmanager.com/gtag/js?id=G-BZTJV1TP3F"></script>
-    <script  >
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-BZTJV1TP3F');
-    </script>
 </head>
     <body>
 
