d00b41f252171a6b62595b386a0e79a861194f76
cisco974 <vitryfrancois@gmail.com>
2024-04-08 09:10:18 +0400

    update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,24 +1,21 @@
-
-
-
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary header ">
     <div class="container-fluid   d-flex justify-content-between">
-        <a  class="navbar-brand  " href="/" alt="TheCasinos.com">
-            <img loading="lazy" id="logo" src="/img/logo-dark.png" alt="The Casinos">
+        <a class="navbar-brand  " href="/" alt="TheCasinos.com">
+            <img loading="lazy" id="logo" src="/img/logo-dark.webp" alt="The Casinos">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".nav_item" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".nav_item"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         @if(Route::currentRouteNamed('index') ||  Route::currentRouteNamed('casino')  || Route::currentRouteNamed('category'))
-        <div class=" search-bar collapse navbar-collapse  nav_item " >
-            <input id="search-casino" class="align-middle  search-bar  "  placeholder="Search..."/>
-            <ul id="search-casino-list" class="search-casino-list d-none"></ul>
-            <button class="btn search-btn">
-                <img  loading="lazy" src="/img/icons/zoom.png" alt="Search" />
-            </button>
-        </div>
+            <div class=" search-bar collapse navbar-collapse  nav_item ">
+                <input id="search-casino" class="align-middle  search-bar  " placeholder="Search..."/>
+                <ul id="search-casino-list" class="search-casino-list d-none"></ul>
+                <button class="btn search-btn">
+                    <img loading="lazy" src="/img/icons/zoom.png" alt="Search"/>
+                </button>
+            </div>
         @endif
         <!-- <div class="language collapse navbar-collapse  nav_item ">
             <div class="dropdown">
@@ -35,13 +32,13 @@
 
         <div class=" collapse navbar-collapse nav_item  " role="search">
             @if (Route::currentRouteNamed('online'))
-                <a href="{{ route('index')}}"  class="btn-online form-control">ONLINE</a>
+                <a href="{{ route('index')}}" class="btn-online form-control">ONLINE</a>
             @else
                 <a href="{{ route('online')}}" class="btn-online form-control">ONLINE</a>
             @endif
-</div>
+        </div>
 
-</div>
+    </div>
 </nav>
 
 
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -6,21 +6,39 @@
         </div>
     @endif
 
-	<div class="casino-block-orange">@if($casino->cat_tablegames)<h3><i class="fa-solid fa-dice"></i>Table Games</h3>@endif
-        @if($casino->cat_poker)<h3><i class="fa-solid fa-coins"></i>Poker Tables</h3>@endif
-        @if($casino->cat_slotmachines)<h3><i class="fa-solid fa-dollar-sign"></i>Gaming Machine</h3>@endif
-        @if($casino->cat_sportsbook)<h3><i class="fa-solid fa-basketball"></i>Sports Book</h3>@endif
-        @if($casino->cat_horseracing)<h3><i class="fa-solid fa-horse"></i>Horse Racing</h3>@endif
-        @if($casino->cat_simulcasting)<h3><i class="fa-solid fa-screencast"></i>Simulcast</h3>@endif
-        @if($casino->cat_offtrack)<h3><i class="fa-solid fa-shuffle"></i>Off Track</h3>@endif
-        @if($casino->cat_greyhounds)<h3><i class="fa-solid fa-dog"></i>Greyhounds</h3>@endif
-        @if($casino->cat_bingo)<h3><i class="fa-solid fa-table-list"></i>Bingo</h3>@endif
-        @if($casino->cat_slotmachines)<h3><i class="fa-solid fa-circle-dollar-to-slot"></i>Slot Machines</h3>@endif</div>
+    <div class="casino-block-orange">@if($casino->cat_tablegames)
+            <h3><i class="fa-solid fa-dice"></i>Table Games</h3>
+        @endif
+        @if($casino->cat_poker)
+            <h3><i class="fa-solid fa-coins"></i>Poker Tables</h3>
+        @endif
+        @if($casino->cat_slotmachines)
+            <h3><i class="fa-solid fa-dollar-sign"></i>Gaming Machine</h3>
+        @endif
+        @if($casino->cat_sportsbook)
+            <h3><i class="fa-solid fa-basketball"></i>Sports Book</h3>
+        @endif
+        @if($casino->cat_horseracing)
+            <h3><i class="fa-solid fa-horse"></i>Horse Racing</h3>
+        @endif
+        @if($casino->cat_simulcasting)
+            <h3><i class="fa-solid fa-screencast"></i>Simulcast</h3>
+        @endif
+        @if($casino->cat_offtrack)
+            <h3><i class="fa-solid fa-shuffle"></i>Off Track</h3>
+        @endif
+        @if($casino->cat_greyhounds)
+            <h3><i class="fa-solid fa-dog"></i>Greyhounds</h3>
+        @endif
+        @if($casino->cat_bingo)
+            <h3><i class="fa-solid fa-table-list"></i>Bingo</h3>
+        @endif
+        @if($casino->cat_slotmachines)
+            <h3><i class="fa-solid fa-circle-dollar-to-slot"></i>Slot Machines</h3>
+        @endif</div>
 
 
-
-
-	<div class="casino-block-purple">
+    <div class="casino-block-purple">
         @if($casino->self_parking)
             <h3><i class="fa-solid fa-square-parking"></i>Self parking</h3>
         @endif
@@ -40,45 +58,56 @@
         @if($casino->spas)
             <h3><i class="fa-solid fa-spa"></i>Spas</h3>
         @endif</div>
-	<div class="casino-block" id="sumup">
-		<h3>To sum up</h3>
+    <div class="casino-block" id="sumup">
+        <h3>To sum up</h3>
 
-        <img loading="lazy"
+        <!--  <img loading="lazy"
              src="{{ env('APP_URL') . '/img/casino/mobile/' . $casino->img_url }}"
 
              srcset="{{ env('APP_URL') . '/img/casino/mobile/' . $casino->img_url }} 480w,
              {{ env('APP_URL') . '/img/casino/tablet/' . $casino->img_url }} 768w,
              {{ env('APP_URL') . '/img/casino/desktop/' . $casino->img_url }} 1024w"
              sizes="(max-width: 480px) 480px, (max-width: 768px) 768px, 1024px" alt="Casino presentation" class=" image-casino ">
+-->
+        <picture>
+            <source media="(min-width: 768px)"
+                    srcset="{{ env('APP_URL') . '/img/casino/desktop/' . $casino->img_url }}">
+            <source media="(max-width: 767px)"
+                    srcset="{{ env('APP_URL') . '/img/casino/mobile/' . $casino->img_url }}" width="300"
+                    height="200">
+            <img loading="lazy" class="rounded-3 img-fluid image-casino center-image"
+                 src="{{ env('APP_URL') . '/img/casino/desktop/' . $casino->img_url }}"
+                 alt="{{ $casino->name }}">
+        </picture>
 
+        <p class="pt-3">{!!  $casinoDetail->sumup!!}</p>
+    </div>
 
+    <div class="casino-block" id="games">
+        <h3>Games</h3>
+        <img loading="lazy" src="/img/sidebar-games.webp" alt="Casino Games" class="image-casino">
+        <p class="pt-3">{!!  $casinoDetail->games!!}</p>
+    </div>
 
-		<p class="pt-3">{!!  $casinoDetail->sumup!!}</p>
-	</div>
+    <div class="casino-block" id="funfacts">
+        <h3>Fun Facts</h3>
+        <img loading="lazy" src="/img/sidebar-facts.webp" alt="Casino Fun facts" class=" image-casino">
+        <p class="pt-3">{!!  $casinoDetail->fun_facts!!}</p>
+    </div>
 
-	<div class="casino-block" id="games">
-		<h3>Games</h3>
-		<img loading="lazy"  src="/img/sidebar-games.jpg" alt="Casino Games" class="image-casino">
-		<p class="pt-3">{!!  $casinoDetail->games!!}</p>
-	</div>
-
-	<div class="casino-block" id="funfacts">
-		<h3>Fun Facts</h3>
-		<img  loading="lazy"    src="/img/sidebar-facts.jpg" alt="Casino Fun facts" class=" image-casino">
-		<p class="pt-3">{!!  $casinoDetail->fun_facts!!}</p>
-	</div>
-
-	<div class="casino-block" id="contact">
-		<h3>Contact</h3>
-		<img  loading="lazy"   src="/img/sidebar-contact.jpg" alt="Casino contact" class=" image-casino">
-        <br>  <br>
-		<p>
+    <div class="casino-block" id="contact">
+        <h3>Contact</h3>
+        <img loading="lazy" src="/img/sidebar-contact.webp" alt="Casino contact" class=" image-casino">
+        <br> <br>
+        <p>
             @if($casino->telephone)
-                <i class="fa-solid fa-phone"></i> &nbsp; <a href="tel:{{  $casino->telephone   }}">{{  $casino->telephone   }}</a>
+                <i class="fa-solid fa-phone"></i> &nbsp; <a
+                    href="tel:{{  $casino->telephone   }}">{{  $casino->telephone   }}</a>
                 <br>
             @endif
             @if($casino->email)
-                <i class="fa-solid fa-envelope"></i> &nbsp; <a href="mailto:{{  $casino->email   }}">{{  $casino->email   }}</a>
+                <i class="fa-solid fa-envelope"></i> &nbsp; <a
+                    href="mailto:{{  $casino->email   }}">{{  $casino->email   }}</a>
                 <br>
             @endif
             @if($casino->adresse)
@@ -86,10 +115,11 @@
                 <br>
             @endif
             @if($casino->website)
-                <i class="fa-solid fa-globe"></i> &nbsp;<a href="{{  $casino->website   }}" target="_blank">Casino Website</a>
+                <i class="fa-solid fa-globe"></i> &nbsp;<a href="{{  $casino->website   }}" target="_blank">Casino
+                    Website</a>
                 <br>
             @endif
         </p>
-	</div>
+    </div>
 </aside>
 
