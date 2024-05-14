6478bf68d0c745c1812d198bbb20623809688765
cisco974 <vitryfrancois@gmail.com>
2024-05-14 10:59:48 +0400

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,9 @@
 @extends('layout')
 @section('page_title', 'TheCasinos.com : Online reference to Onsite experience')
-<h1 style="display: none;">TheCasinos.com - Online reference to onsite experience</h1>
+
 @section('page_description','With TheCasinos.com, enjoy the premier online guide reference. Explore a world of top-tier gaming and enhance your onsite casino journey.')
 @section('map')
+    <h1 style="display: none;">TheCasinos.com - Online reference to onsite experience</h1>
     @include('map')
 @endsection
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta name="google-site-verification" content="cEs10KedajHFcdCI2ltJ2Wo5fqzy5kSrknaMhxYJ0VQ"/>
-
+    <link rel="icon" href="{{ asset('favicon.ico') }}favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <link rel="canonical" href="{{ config('app.url') . parse_url(request()->getRequestUri(), PHP_URL_PATH) }}">
 
@@ -13,10 +14,12 @@
     @yield('meta-tags')
 
 
-    @vite("resources/js/app.js")
     <title>@yield('page_title')</title>
     <meta name="description" content="@yield('page_description')">
     <meta name="keywords" content="@yield('page_keywords')">
+
+
+    @vite("resources/js/app.js")
     @yield('context-js')
     <meta name="yandex-verification" content="9167145b8ec2988b"/>
     <!-- 2024 Google tag (gtag.js) -->
