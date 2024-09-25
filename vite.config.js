d6f9622add4eeaf008a2b704f8bb0a8760089be3
cisco974 <vitryfrancois@gmail.com>
2024-09-25 09:33:18 +0400

    update perf

--- vite.config.js
+++ vite.config.js
@@ -15,17 +15,7 @@ export default defineConfig({
         }),
     ],
     build: {
-        rollupOptions: {
-            output: {
-                // Diviser les dépendances des node_modules en chunks séparés
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                },
-            },
-        },
-        // Augmentez le nombre de workers ou la mémoire pour gérer les gros fichiers
-        chunkSizeWarningLimit: 1000, // Ajustez la limite de taille de chunk si nécessaire
-    },
+        cache: true,
+
+    }
 });
